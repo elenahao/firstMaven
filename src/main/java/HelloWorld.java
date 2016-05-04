fa07faf1c0cbff321b658daabfc8d8115cc5f84e
haoxueying <elena_haoo@163.com>
2016-05-04 09:47:37 +0800

    some changes

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by HaoXueying on 2016/5/3.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("Hello World!!!!!!!!!!!!!");
+    }
+}
